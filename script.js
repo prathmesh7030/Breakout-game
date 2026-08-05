c352bc2b5f52e62bc20e41a169e7cde22209a7a0
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-05 21:58:29 +0530

    Move paddle on canvas

--- script.js
+++ script.js
@@ -89,3 +89,17 @@ function drawBricks() {
     });
   });
 }
+
+// Move paddle on canvas
+function movePaddle() {
+  paddle.x += paddle.dx;
+
+  // Wall detection
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+
+  if (paddle.x < 0) {
+    paddle.x = 0;
+    }
+}
