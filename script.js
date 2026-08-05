0c040e7a37950071c3894661937e643487297201
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-05 21:40:05 +0530

    Create bricks

--- script.js
+++ script.js
@@ -42,3 +42,14 @@ const brickInfo = {
   visible: true
 };
 
+// Create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
