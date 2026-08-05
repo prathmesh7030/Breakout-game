8dcefc6ab237994ea56746f1e824f7cd4bdaa745
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-05 21:26:00 +0530

    access elements

---
+++ script.js
@@ -0,0 +1,11 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
+const delay = 500; //delay to reset the game
