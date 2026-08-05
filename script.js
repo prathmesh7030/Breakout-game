d91ba69f179025ee59589517a6f246279eb1f905
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-05 22:07:24 +0530

    Increase score

--- script.js
+++ script.js
@@ -148,3 +148,33 @@ function moveBall() {
       }
     });
   });
+
+ // Hit bottom wall - Lose
+  if (ball.y + ball.size > canvas.height) {
+    showAllBricks();
+    score = 0;
+  }
+}
+
+// Increase score
+function increaseScore() {
+  score++;
+
+  if (score % (brickRowCount * brickColumnCount) === 0) {
+
+      ball.visible = false;
+      paddle.visible = false;
+
+      //After 0.5 sec restart the game
+      setTimeout(function () {
+          showAllBricks();
+          score = 0;
+          paddle.x = canvas.width / 2 - 40;
+          paddle.y = canvas.height - 20;
+          ball.x = canvas.width / 2;
+          ball.y = canvas.height / 2;
+          ball.visible = true;
+          paddle.visible = true;
+      },delay)
+  }
+}
