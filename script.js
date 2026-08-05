49a0a31a40d5a66bd8e13a031847ece9cffd1a93
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-05 21:27:50 +0530

    Create paddle props

--- script.js
+++ script.js
@@ -20,3 +20,14 @@ const ball = {
   dy: -4,
   visible: true
 };
+
+// Create paddle props
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+  visible: true
+};
