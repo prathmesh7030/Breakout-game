eee701dbf7dad79b0615ecdd58dd6acd23419da8
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-05 22:13:58 +0530

    added key events

--- script.js
+++ script.js
@@ -209,3 +209,24 @@ function update() {
 }
 
 update();
+
+// Keydown event
+function keyDown(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    paddle.dx = paddle.speed;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+// Keyup event
+function keyUp(e) {
+  if (
+    e.key === 'Right' ||
+    e.key === 'ArrowRight' ||
+    e.key === 'Left' ||
+    e.key === 'ArrowLeft'
+  ) {
+    paddle.dx = 0;
+  }
+}
