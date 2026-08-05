ef87036522ae4c1fec49ba57aa25a0137ffa6aa0
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-05 22:05:15 +0530

    Brick collision

--- script.js
+++ script.js
@@ -129,3 +129,22 @@ function moveBall() {
   ) {
     ball.dy = -ball.speed;
   }
+
+  // Brick collision
+  bricks.forEach(column => {
+    column.forEach(brick => {
+      if (brick.visible) {
+        if (
+          ball.x - ball.size > brick.x && // left brick side check
+          ball.x + ball.size < brick.x + brick.w && // right brick side check
+          ball.y + ball.size > brick.y && // top brick side check
+          ball.y - ball.size < brick.y + brick.h // bottom brick side check
+        ) {
+          ball.dy *= -1;
+          brick.visible = false;
+
+          increaseScore();
+        }
+      }
+    });
+  });
